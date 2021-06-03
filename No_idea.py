fdde447691742e8a992238237e98d647990e68eb
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2021-06-04 00:04:36 +0530

    Create No_idea.py

---
+++ No_idea.py
@@ -0,0 +1,49 @@
+Input Format
+
+The first line contains integers  and  separated by a space.
+The second line contains  integers, the elements of the array.
+The third and fourth lines contain  integers,  and , respectively.
+
+Output Format
+
+Output a single integer, your total happiness.
+
+Sample Input
+
+3 2
+1 5 3
+3 1
+5 7
+Sample Output
+
+1Input Format
+
+The first line contains integers n and m separated by a space.
+The second line contains n integers, the elements of the array.
+The third and fourth lines contain m integers, A and B, respectively.
+
+Output Format
+
+Output a single integer, your total happiness.
+
+Sample Input
+
+3 2
+1 5 3
+3 1
+5 7
+Sample Output
+
+1
+
+Solution:
+n,m = map(int,input().split())
+N = list(map(int,input().split()))
+A = set(map(int,input().split()))
+B = set(map(int,input().split()))
+#Union set A & B
+C = A | B
+#Exclude all numbers which doesn't exit in both A & B
+N = (i for i in N if i in C)
+#Add 1 if number is in set A else subtract 1
+print(sum(1 if i in A else -1 for i in N ))
