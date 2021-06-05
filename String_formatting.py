7c49dea924c7a623ccaecd5a00e060d2db4214c4
Arvind Rao <47332035+arvind73@users.noreply.github.com>
2021-06-05 15:26:44 +0530

    Create String_formatting.py

---
+++ String_formatting.py
@@ -0,0 +1,39 @@
+Given an integer, , print the following values for each integer i from 1 to n:
+
+Decimal
+Octal
+Hexadecimal (capitalized)
+Binary
+
+Sample Input
+
+17
+Sample Output
+
+    1     1     1     1
+    2     2     2    10
+    3     3     3    11
+    4     4     4   100
+    5     5     5   101
+    6     6     6   110
+    7     7     7   111
+    8    10     8  1000
+    9    11     9  1001
+   10    12     A  1010
+   11    13     B  1011
+   12    14     C  1100
+   13    15     D  1101
+   14    16     E  1110
+   15    17     F  1111
+   16    20    10 10000
+   17    21    11 10001
+    
+Solution:
+def print_formatted(number):
+    # your code goes here
+    width = len("{0:b}".format(n)) 
+    for i in range(1,n+1):
+        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
